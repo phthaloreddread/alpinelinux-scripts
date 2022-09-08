8ae80e994127fa063945de7d2d74d09422919fca
Gellert Berenyi <berenyi.gellert@gmail.com>
2022-09-07 23:47:23 -0400

    Update add-essentials.sh
    adding passgen

--- add-essentials.sh
+++ add-essentials.sh
@@ -1,3 +1,7 @@
 #!/bin/ash
 
 apk add coreutils curl exa git hunspell man-db screen tmux ttf-droid wget vim
+
+wget https://raw.githubusercontent.com/gellertb97/passgen20/main/alpine/passgen20.sh
+mv ./passgen20.sh /usr/bin/
+chmod a+x /usr/bin/passgen20.sh
