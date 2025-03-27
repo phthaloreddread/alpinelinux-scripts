c7fb58a622f1568c8a6a57caeb4db6eff8165e10
phthaloreddread <berenyi.gellert@gmail.com>
2025-03-26 19:29:57 -0600

    Update add-essentials.sh
    Added lsblk, lscpu, lsof

--- add-essentials.sh
+++ add-essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-apk add coreutils curl exa git hunspell man-db screen tmux ttf-droid wget vim
+apk add coreutils lsblk lscpu lsof curl exa git hunspell man-db screen tmux ttf-droid wget vim
 
 wget https://raw.githubusercontent.com/gellertb97/passgen20/main/alpine/passgen20.sh
 mv ./passgen20.sh /usr/bin/
