27fddfbedf7f9265672ed231bb2ef2588cbc7a10
phthaloreddread <berenyi.gellert@gmail.com>
2025-03-26 20:00:20 -0600

    Update add-essentials.sh
    Add section to backup existing /etc/passwd, and change /bin/sh or /bin/ash to /bin/bash.

--- add-essentials.sh
+++ add-essentials.sh
@@ -1,7 +1,11 @@
-#!/bin/ash
+#!/bin/sh
 
-apk add coreutils lsblk lscpu lsof curl exa git hunspell man-db screen tmux ttf-droid wget vim
+apk add bash coreutils lsblk lscpu lsof curl exa git htop hunspell linux-pam man-db libxcb musl ncurses screen tmux tput ttf-droid wget vim
 
 wget https://raw.githubusercontent.com/gellertb97/passgen20/main/alpine/passgen20.sh
 mv ./passgen20.sh /usr/bin/
 chmod a+x /usr/bin/passgen20.sh
+
+cp /etc/passwd /etc/passwd.bak
+sed -i "s/\/bin\/ash/\/bin\/bash/g" /etc/passwd
+sed -i "s/\/bin\/sh/\/bin\/bash/g" /etc/passwd
